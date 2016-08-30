5dbcccd882631a48623fd40c6ca925ffb1da7d0e
Zachary Berardo <zachary@zachnthebox.com>
2016-08-29 22:16:48 -0400

    fix: small tweaks

--- app/components/spotify-playlist/component.js
+++ app/components/spotify-playlist/component.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['spotify-play--playlist'],
+  classNames: ['card', 'spotify-play--playlist'],
 
   loading: true,
 
--- app/playlists/route.js
+++ app/playlists/route.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').findAll('playlist');
+    return this.get('store').findAll('playlist').then(playlists => {
+      return playlists.sortBy('snapshotDate').reverse();
+    });
   },
 });
